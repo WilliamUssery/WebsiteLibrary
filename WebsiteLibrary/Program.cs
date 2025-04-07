130dce5f56cdd5336ebe0ce30640342fbeb01359
WilliamUssery <william.ussery1@mga.edu>
2025-04-07 12:27:31 -0400

    Adding the Main Menu to navigate the CRUD Programs.

--- WebsiteLibrary/Program.cs
+++ WebsiteLibrary/Program.cs
@@ -18,7 +18,48 @@ namespace WebsiteLibrary
     {
         static void Main(string[] args)
         {
-            
+            static List<Website> websites = new List<Website>(); // IDK why this is red
+
+            static void Main(string[] args) // Not being used yet
+            {
+                bool running = true;
+
+                while (running)
+                {
+                    Console.WriteLine("\n Website Library");
+                    Console.WriteLine("1. Add Website");
+                    Console.WriteLine("2. View Websites");
+                    Console.WriteLine("3. Update Website");
+                    Console.WriteLine("4. Delete Website");
+                    Console.WriteLine("5. Exit");
+                    Console.Write("Choose an option: ");
+
+                    string input = Console.ReadLine();
+
+                    switch (input)
+                    {
+                        case "1":
+                            AddWebsite(); // Need to add all these functions later. That's why they are red
+                            break;
+                        case "2":
+                            ViewWebsites();
+                            break;
+                        case "3":
+                            UpdateWebsite();
+                            break;
+                        case "4":
+                            DeleteWebsite();
+                            break;
+                        case "5":
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option.");
+                            break;
+                    }
+                }
+            }
+
 
         }
     }
