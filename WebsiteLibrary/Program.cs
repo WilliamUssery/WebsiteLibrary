f01d4f02c301e81f2e79f9e16150b1f9f3f39b51
WilliamUssery <william.ussery1@mga.edu>
2025-04-07 12:19:10 -0400

    Remove Hello World & Adding a website class

--- WebsiteLibrary/Program.cs
+++ WebsiteLibrary/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace WebsiteLibrary
 {
+    internal class Website
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public int Rating { get; set; } 
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
         }
     }
 }
